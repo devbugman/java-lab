2526668a6da5e02ef29c0540c888119c5ee5ca17
user <cksqhr4961@naver.com>
2022-08-04 15:51:25 +0900

    ComponentScan

---
+++ spring-core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,29 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+
+//        basePackages : 탐색할 패키지의 시작 위치를 지정한다. 이 패키지를 포함해서 하위 패키지를 모두
+//        탐색한다.
+//        basePackages = {"hello.core", "hello.service"} 이렇게 여러 시작 위치를 지정할 수도
+//        있다.
+//        basePackageClasses : 지정한 클래스의 패키지를 탐색 시작 위치로 지정한다.
+//        만약 지정하지 않으면 @ComponentScan 이 붙은 설정 정보 클래스의 패키지가 시작 위치가 된다
+// 수동빈이 우선권을 가짐(수동빈이 자동빈을 오버라이딩을 해 버린다.)
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
