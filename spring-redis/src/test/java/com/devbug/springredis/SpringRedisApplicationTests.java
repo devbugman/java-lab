5843bf78876bb50d98dfef5cb466b696355010be
devbugman <cksqhr4961@naver.com>
2022-12-05 10:26:28 +0900

    feat: setting spring-redis

---
+++ spring-redis/src/test/java/com/devbug/springredis/SpringRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbug.springredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
