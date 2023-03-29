ac617a6dd77bd7c7993a0965e4365bc0a4824233
devbugman <cksqhr4961@naver.com>
2023-03-29 18:42:49 +0900

    item 28 배열대신 리스트

---
+++ effective-java/src/main/java/chapter5/ArrayChooser.java
@@ -0,0 +1,18 @@
+package chapter5;
+
+import java.util.Collection;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ArrayChooser {
+    private final Object[] choiceArray;
+
+    public ArrayChooser(Collection choices) {
+        choiceArray = choices.toArray();
+    }
+
+    public Object choose() {
+        Random rnd = ThreadLocalRandom.current();
+        return choiceArray[rnd.nextInt(choiceArray.length)];
+    }
+}
---
+++ effective-java/src/test/java/chapter5/Item28Test.java
@@ -0,0 +1,49 @@
+package chapter5;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("배열보다는 리스트")
+public class Item28Test {
+
+    @Test
+    @DisplayName("배열은 런타임에 실패한다 [ArrayStoreException]")
+    void array1() {
+        Object[] objectArray = new Long[1];
+        assertThatThrownBy(() -> objectArray[0] = "str")
+                .isInstanceOf(ArrayStoreException.class);
+    }
+
+    @Test
+    @DisplayName("제네릭은 컴파일이 되지 않는다")
+    void generic1() {
+//        List<Object> objects = new ArrayList<Long>(); //컴파일 에러
+        List<Object> objects = new ArrayList<Object>();
+    }
+
+    @Test
+    void array2() {
+        List list = new ArrayList();
+        list.add(1);
+        list.add("hello");
+
+        ArrayChooser arrayChooser = new ArrayChooser(list);
+        assertThatThrownBy(() -> {
+            Integer choose = (Integer) arrayChooser.choose();
+        }).isInstanceOf(ClassCastException.class);
+    }
+
+    @Test
+    void generic2() {
+        List<String> list = List.of("hello", "java", "world");
+
+        GenericChooser<String> chooser = new GenericChooser<>(list);
+        assertThat(list).contains(chooser.choose());
+    }
+}
